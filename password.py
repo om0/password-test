b98313a2ca439e9edc501730925cde48541377d6
om0 <leo.needajob@gmail.com>
2018-08-09 16:50:15 +0800

    final

--- password.py
+++ password.py
@@ -1,11 +1,12 @@
 password = 'a123456'
 i = 3 #剩餘次數
 while i > 0:
+	i = i - 1
 	pwd = input('請輸入密碼')
 	if pwd == password:
 		print('登入成功')
 		break
-	else:
-		i = i - 1
+	elif i > 0:
 		print('密碼錯誤！ 還有', i, '次機會')
-	
+	else:
+		print('密碼輸入超過次數! bye!')
